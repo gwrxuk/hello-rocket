e8a14867cf058669a2b1d68c0eb359ebfd4cb755
gwrxuk <gwrx2005@gmail.com>
2024-02-13 14:47:35 +0800

    Add graphql

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate juniper;
+
+use juniper::{FieldResult};
+
+#[derive(GraphQLEnum)]
+enum Product {
+    MP3,
+    MP4,
+    Image,
+}
+
+#[derive(GraphQLObject)]
+#[graphql(description="Shopping cart")]
+struct ShoppingCart {
+    id: String,
+    user_id: String,
+    product_ids: Vec<Product>,
+    updated_at: String,
+}
 
 #[get("/")]
 fn index()->&'static str {
